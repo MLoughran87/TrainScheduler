20bfe43132fd88ddcca6e811372534ce807210fa
Locke Monstah <Mloughran87@gmail.com>
2018-10-30 15:53:25 -0400

    Html and CSS done

---
+++ assets/trains.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBSPyZXZHH64wZLsSgNNt7HwSOq3HGKFKw",
+    authDomain: "homework7-26fbb.firebaseapp.com",
+    databaseURL: "https://homework7-26fbb.firebaseio.com",
+    projectId: "homework7-26fbb",
+    storageBucket: "homework7-26fbb.appspot.com",
+    messagingSenderId: "520656377702"
+  };
+  firebase.initializeApp(config);
